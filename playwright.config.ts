d20bff5e1b76667ddb441068a13baa3d34a5bbc0
Haikal <muhammad.haikal@hitachi-ch.co.id>
2026-07-08 11:39:40 +0700

    OrangeHRM automation framework with Playwright TypeScript

--- playwright.config.ts
+++ playwright.config.ts
@@ -14,7 +14,7 @@ export default defineConfig({
 
   use: {
     baseURL: 'https://opensource-demo.orangehrmlive.com',
-    headless: false,
+    headless: !!process.env.CI,
     screenshot: 'only-on-failure',
     video: 'retain-on-failure',
     trace: 'on-first-retry',
